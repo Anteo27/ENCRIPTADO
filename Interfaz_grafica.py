1e4dddc36105432456cc1bf55d00af4d4e007530
Anteo <harleytriumphdavidson@gmail.com>
2023-10-15 19:43:14 +0200

    rediseño de la interfaz grafica

--- Interfaz_grafica.py
+++ Interfaz_grafica.py
@@ -1,10 +1,9 @@
 import tkinter as tk
 from tkinter import filedialog, ttk, simpledialog, messagebox
 from ttkthemes import ThemedTk
-import encriptar 
+import encriptar
 import os
 ruta=""
-
 def cifrar():
     global ruta
     if not ruta:
@@ -20,11 +19,11 @@ def cifrar():
     if "Clave_256.txt" not in filekey:
          messagebox.showwarning("Advertencia", "Por favor, selecciona un fichero valido de clave.")
          return
-    clave=b''
+    respuesta=b''
     with open(filekey, 'rb') as infile:
             lectura=infile.read()
-            clave+=lectura
-    encriptar.encrypt_file(ruta,clave)
+            respuesta+=lectura
+    encriptar.encrypt_file(ruta,respuesta)
     entrada.state(['!readonly'])
     entrada.delete(0, tk.END)
     entrada.state(['readonly'])
@@ -44,17 +43,15 @@ def descifrar():
     if "Clave_256.txt" not in filekey:
          messagebox.showwarning("Advertencia", "Por favor, selecciona un fichero valido de clave.")
          return
-    clave=b''
+    respuesta=b''
     with open(filekey, 'rb') as infile:
             lectura=infile.read()
-            clave+=lectura
-    encriptar.decrypt_file(ruta,clave)
+            respuesta+=lectura
+    encriptar.decrypt_file(ruta,respuesta)
     entrada.state(['!readonly'])
     entrada.delete(0, tk.END)
     entrada.state(['readonly'])
-
 nombre_Archivo=""
-
 def seleccionar_archivo():
     global nombre_Archivo
     # Abre un cuadro de diálogo para seleccionar un archivo
@@ -81,24 +78,21 @@ ventana.title('Cifrador y Descifrador')
 marco = ttk.Frame(ventana, padding="100")
 marco.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
 
-# Crea un botón para seleccionar un archivo
-boton_seleccionar_archivo = ttk.Button(marco, text="Seleccionar archivo", command=seleccionar_archivo)
-boton_seleccionar_archivo.grid(row=0, column=0, sticky=tk.W, padx=7, pady=5)
-
 # Crea un widget de entrada de texto
 entrada = ttk.Entry(marco, text=nombre_Archivo)
-entrada.grid(row=1, column=0, sticky=(tk.W, tk.E))
+entrada.grid(row=0, column=0, sticky=(tk.W, tk.E))
 entrada.state(['readonly'])
 
-etiqueta = ttk.Label(marco, text="")
-etiqueta.grid(row=2, column=0, sticky=(tk.W, tk.E))
-
 # Crea los botones de cifrar y descifrar
-boton_cifrar = ttk.Button(marco, text="Cifrar y elegir clave", command=confirmar_cifrado)
-boton_cifrar.grid(row=4, column=0, sticky=tk.W, padx=5, pady=5)
+boton_cifrar = ttk.Button(marco, text="Cifrar", command=confirmar_cifrado)
+boton_cifrar.grid(row=1, column=0, sticky=tk.W, padx=5, pady=5)
 
-boton_descifrar = ttk.Button(marco, text="Descifrar y elegir clave", command=descifrar)
-boton_descifrar.grid(row=4, column=1, sticky=tk.W, padx=5, pady=5)
+boton_descifrar = ttk.Button(marco, text="Descifrar", command=descifrar)
+boton_descifrar.grid(row=2, column=0, sticky=tk.W, padx=5, pady=5)
+
+# Crea un botón para seleccionar un archivo
+boton_seleccionar_archivo = ttk.Button(marco, text="Seleccionar archivo", command=seleccionar_archivo)
+boton_seleccionar_archivo.grid(row=3, column=0, sticky=tk.W, padx=5, pady=5)
 
 # Configura el marco para expandirse con la ventana
 ventana.columnconfigure(0, weight=1)
